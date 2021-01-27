51fcbab938dc70f6d5ecc307fc1a9dd621d21093
k-marczak <k_marczak@outlook.com>
2021-01-27 01:26:36 +0100

    add about components

--- src/About.js
+++ src/About.js
@@ -1,64 +1,84 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
-import image1 from './assets/it.jpg'
+import { Jumbotron, Button, Container, Alert, Toast  } from 'react-bootstrap';
+import image from './assets/image.jpg'
 import image2 from './assets/it2.jpg'
 import image3 from './assets/it4.jpg'
 
 import styled from 'styled-components'
 
 const Styles = styled.div`
-    .carousel{
-        margin-top: 100px;
-        margin-bottom: 100px;
+    
+
+    .container{
+        width: 100vw;
     }
 
-    .carousel-item img{
-        height: 500px;
+    .alert{
+        position: relative;
+        top: 520px;
     }
-`
+
+
+    .jumbotron{
+        position: absolute;
+        width: 100vw;
+        left: 0;
+        right: 0;
+        height: 400px;
+        background: url(${image}) no-repeat fixed bottom;
+        background-size: cover;
+        z-index: -2;
+    }
+
+`;
 
 export const About = () => {
     return (
         <Styles>
-            <Carousel className="carousel">
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={image1}
-                    alt="First slide"
-                    />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={image2}
-                    alt="Third slide"
-                    />
+            
+            <Jumbotron fluid style={{marginTop: '72px'}}>
+                <div className="overlay"></div>
+                <Container>
+                    <h1 style={{marginTop: '40px'}}> Potrzebujesz pomocy? </h1>
+                    <h2>+48 123 431 432</h2>
+                    <p>
+                    Ruszyła infolinia dedykowana szczepieniom przeciwko COVID-19
+                    </p>
+                    <Button variant="secondary">Więcej...</Button>
+                </Container>
+            </Jumbotron>
 
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-100"
-                    src={image3}
-                    alt="Third slide"
-                    />
+            <Alert variant="info">
+                <Alert.Heading>O szczepionce na dedykowanej infolinii </Alert.Heading>
+                <p>
+                Szczepionka przeciwko COVID-19 to temat, który nasuwa wiele pytań. Zależy nam, aby każda osoba mogła rozwiać swoje wątpliwości i znaleźć odpowiedź na nurtujące pytania. Dlatego też uruchomiliśmy bezpłatną i całodobową infolinię.
+                </p>
+                <hr />
+                <p className="mb-0">
+                Jeśli dzwonisz z zagranicy lub gdy Twój operator nie obsługuje numerów specjalnych, zadzwoń pod numer 22 12 23 212.
+                </p>
+            </Alert>
+            
+            <Toast>
+                <Toast.Header>
+                <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+                <strong className="mr-auto">Bootstrap</strong>
+                <small>just now</small>
+                </Toast.Header>
+                <Toast.Body>See? Just like this.</Toast.Body>
+            </Toast>
+            <Toast>
+                <Toast.Header>
+                <img src="holder.js/20x20?text=%20" className="rounded mr-2" alt="" />
+                <strong className="mr-auto">Bootstrap</strong>
+                <small>2 seconds ago</small>
+                </Toast.Header>
+                <Toast.Body>Heads up, toasts will stack automatically</Toast.Body>
+            </Toast>
 
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                    </Carousel.Caption>
-                </Carousel.Item>
-            </Carousel>
+            
 
-
+            
 
         </Styles>
     );
